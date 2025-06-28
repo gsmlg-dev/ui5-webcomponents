d77895e27e27719693513c8f635452c3153c60cc
gsmlg <gsmlg@users.noreply.github.com>
2025-06-28 00:15:58 +0000

    Deploying to gh-pages from @ gsmlg-dev/ui5-webcomponents@72aae89c43412b585e17f5d552810cad8564d656 🚀

---
+++ nightly/local-cdn/base/dist/generated/VersionInfo.js
@@ -0,0 +1,11 @@
+const VersionInfo = {
+    version: "2.12.0-rc.2",
+    major: 2,
+    minor: 12,
+    patch: 0,
+    suffix: "-rc.2",
+    isNext: false,
+    buildTime: 1751069509,
+};
+export default VersionInfo;
+//# sourceMappingURL=VersionInfo.js.map
---
+++ nightly/version.md
@@ -0,0 +1,5 @@
+commit 72aae89c43412b585e17f5d552810cad8564d656
+Author: ui5-webcomponents-bot <ui5-webcomponents-bot@sap.com>
+Date:   Thu Jun 26 08:10:11 2025 +0000
+
+    chore(release): publish v2.12.0-rc.2 [ci skip]
