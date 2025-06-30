974ffba802603444fcb40dbe4c3cb65266587370
gsmlg <gsmlg@users.noreply.github.com>
2025-06-30 00:16:46 +0000

    Deploying to gh-pages from @ gsmlg-dev/ui5-webcomponents@72aae89c43412b585e17f5d552810cad8564d656 🚀

--- nightly/local-cdn/base/dist/generated/VersionInfo.js
+++ nightly/local-cdn/base/dist/generated/VersionInfo.js
@@ -5,7 +5,7 @@ const VersionInfo = {
     patch: 0,
     suffix: "-rc.2",
     isNext: false,
-    buildTime: 1751156002,
+    buildTime: 1751242374,
 };
 export default VersionInfo;
 //# sourceMappingURL=VersionInfo.js.map
